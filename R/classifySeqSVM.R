a6e2e12e12a79b8e62e87205b9ff538caabc1046
jmal0403 <jmal0403@gmail.com>
2024-01-18 23:04:04 -0700

    Update classifySeqSVM.R

--- R/classifySeqSVM.R
+++ R/classifySeqSVM.R
@@ -1,7 +1,8 @@
 classifySeqSVM <- function(train.df, cost, sigma) {
+  library(e1071)
   # Cost affects performance and should be used in sensitivity analysis
   kernel.type<-'radial'
   classification.machine<-'C-classification'
-  svmfit = svm(formula = as.factor(class) ~ .,data = train.df, cost=cost, type =classification.machine, kernel = kernel.type, probability=TRUE, sigma=sigma)
+  svmfit = e1071::svm(formula = as.factor(class) ~ .,data = train.df, cost=cost, type =classification.machine, kernel = kernel.type, probability=TRUE, sigma=sigma)
   return(svmfit)
 } 
