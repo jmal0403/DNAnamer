c570d0ce54ead410bfcd5a816d2d997ee754ed4f
jmal0403 <jmal0403@gmail.com>
2024-01-18 22:14:07 -0700

    Update loadRefGenome.R

--- R/loadRefGenome.R
+++ R/loadRefGenome.R
@@ -1,7 +1,7 @@
 loadRefGenome <- function(organism.name) {
   library(Biostrings)
   library(biomartr)
-  genome.refseq <- getGenome(db = "refseq", organism = organism.name)
+  genome.refseq <- getGenome(db = "refseq", organism = organism.name, skip_bacteria = TRUE)
   Genome <-  read_genome(file = genome.refseq)
   return(Genome)
 }
