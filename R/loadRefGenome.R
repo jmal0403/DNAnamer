e8a20ac56157d46d9e15ce4276be64451034837f
jmal0403 <jmal0403@gmail.com>
2024-01-18 21:12:33 -0700

    Update loadRefGenome.R

--- R/loadRefGenome.R
+++ R/loadRefGenome.R
@@ -1,4 +1,4 @@
-
+library(biomartr)
 loadRefGenome <- function(organism.name) {
   genome.refseq <- getGenome(db = "refseq", organism = organism.name, path = file.path("_ncbi_downloads","genomes"))
   Genome <-  read_genome(file = genome.refseq)
